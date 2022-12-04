a09ed9a68a6693991b62c4f44913a8ee2934a33e
Makito Katsube <katsubemakito@gmail.com>
2022-12-05 00:20:23 +0900

    マッチング完了までを実装

--- public/js/lib.js
+++ public/js/lib.js
@@ -1,3 +1,20 @@
+/*
+ * 文字数をカウントする
+ */
 function strCount(str){
   return [...str].length;
+}
+
+/**
+ * 入力中の名前をlocalStorageへ保存
+ */
+function saveName(){
+  localStorage.setItem('playername', playername.value);
+}
+
+/**
+ * 名前をlocalStorageから取得
+ */
+function loadName(){
+  return localStorage.getItem('playername');    // 存在しない場合はNullが返る
 }
--- serve.js
+++ serve.js
@@ -14,6 +14,19 @@ const DOCUMENT_ROOT = __dirname + '/public';
 // サーバのポート番号
 const PORT = 3000;
 
+// 参加者のリスト
+const MEMBERS = {
+  // {id:1, token:'xxx', socketid:'yyy', name:'あるぱか'},
+};
+let MEMBERS_COUNT = 1;
+const MAXPLAYERS = 2;   // 最大同時参加者数
+
+// モニタリング間隔（ミリ秒）
+const MONITORING_TIME = 1000;
+
+// 現在のサーバーモード
+let SERVER_MODE = 'MATCIHING';  // 'MATCIHING' or 'GAME'
+
 //-----------------------------------------------
 // ルーティング（express）
 //-----------------------------------------------
@@ -30,6 +43,42 @@ app.get('/:dir/:file', (req, res) => {
   res.sendFile(`${DOCUMENT_ROOT}/${dir}/${file}`);
 });
 
+//-----------------------------------------------
+// Socket.IO
+//-----------------------------------------------
+io.on('connection', (socket) => {
+  console.log('新しいユーザーが接続しました');
+
+  //---------------------------
+  // 新規接続
+  //---------------------------
+  // トークンを生成してクライアントに送信
+  const token = createToken();
+  io.to(socket.id).emit('token', token);
+
+  // 参加者リストに追加
+  MEMBERS[socket.id] = {
+    id: MEMBERS_COUNT,
+    token: token,
+    name: null
+  };
+  MEMBERS_COUNT++;
+
+  //---------------------------
+  // 入室
+  //---------------------------
+  socket.on('join', (data) => {
+    console.log('join', data);
+    // トークンを検証
+    if(data.token !== MEMBERS[socket.id].token){
+      console.log('トークンが一致しません');
+      return;
+    }
+
+    // 参加者リストに追加
+    MEMBERS[socket.id].name = data.name;
+  });
+});
 
 //------------------------------
 // サーバ起動
@@ -37,3 +86,49 @@ app.get('/:dir/:file', (req, res) => {
 http.listen(PORT, () => {
   console.log(`サーバを起動しました localhost:${PORT}`);
 });
+
+//------------------------------
+// タイマー
+//------------------------------
+setInterval(() => {
+  // マッチング中
+  if(SERVER_MODE === 'MATCIHING'){
+    // 2人以上いたらゲーム開始
+    if(countConnectMembers() >= MAXPLAYERS){
+      console.log('ゲーム開始');
+      SERVER_MODE = 'GAME';
+      io.emit('start');
+    }
+  }
+  // ゲーム中
+  else if(SERVER_MODE === 'GAME'){
+    // ToDo: 次回以降追加
+  }
+}, MONITORING_TIME);
+
+
+/**
+ * 現在の参加者数をカウント
+ *
+ * @returns {number}
+ */
+function countConnectMembers(){
+  let count = 0;
+  for(let key in MEMBERS){
+    if(MEMBERS[key].name !== null){
+      count++;
+    }
+  }
+  return(count);
+}
+
+/**
+ * トークンを生成する
+ *
+ * @return {string} トークン
+ */
+function createToken(){
+  const uuid = crypto.randomUUID();       // 例： 1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed
+  const token = uuid.replaceAll('-','');  // 例： 1b9d6bcdbbfd4b2d9b5dab8dfbbd4bed
+  return(token);
+}
